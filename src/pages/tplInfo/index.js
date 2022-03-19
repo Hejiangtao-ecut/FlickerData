ba49b6b5ba4219026893f67e367219314b8f06dd
Hejiangtao-ecut <1471867575@qq.com>
2022-03-19 23:59:10 +0800

    finish get chart

--- src/pages/tplInfo/index.js
+++ src/pages/tplInfo/index.js
@@ -8,15 +8,24 @@ import { saveImageToPhotos, initChart } from '../../common/js/canvas';
 Page({
     data: {
         ec: {
-            onInit:initChart
+            onInit: initChart
         },
         clicked: false
+    },
+
+    demo(e) {
+        // 获取 chart，进行数据操纵
+        console.log('demo----');
+        console.log(e);
     },
 
     /**
      * 保存图片至本地
      */
     save() {
-        saveImageToPhotos(this.selectComponent('#mychart-dom-bar'));
+        // saveImageToPhotos(this.selectComponent('#mychart-dom-bar'));
+
+        const x = wx.createSelectorQuery().in(this);
+        console.log(x.selectAll('.container'));
     }
 });
